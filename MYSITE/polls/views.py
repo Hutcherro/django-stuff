4419058659e9e0762eaed04f6faeb8c643e5b01e
hutcherro <hutcherro@gmail.com>
2020-06-05 22:46:52 +0200

    done tutorial

--- MYSITE/polls/views.py
+++ MYSITE/polls/views.py
@@ -2,9 +2,44 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.urls import reverse
 #from django.template import loader
+from django.views import generic
+from django.utils import timezone
+from django.views.decorators.http import require_http_methods
 
 from .models import Question, Choice
 
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list'
+
+	def get_queryset(self):
+		return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls/detail.html'
+	def get_queryset(self):
+		return Question.objects.filter(pub_date__lte=timezone.now())
+
+class ResultView(generic.DetailView):
+	model = Question
+	template_name = 'polls/result.html'
+
+@require_http_methods(["GET", "POST"])
+def vote(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		return render(request, 'polls/detail.html', {
+			'question':question,
+			'error message': "You didn't select a choice.",
+			})
+	else:
+		return HttpResponseRedirect(reverse('polls:result', args=(question.id,)))
+
+
+""" 
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
 	#output = ', '.join([q.question_text for q in latest_question_list])
@@ -35,4 +70,4 @@ def vote(request, question_id):
 			})
 	else:
 		return HttpResponseRedirect(reverse('polls:result', args=(question.id,)))
-# Create your views here.
+"""
